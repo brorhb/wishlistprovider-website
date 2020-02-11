3f7c13f9443f2c3702b258f6411ca568a3ee234c
bror <@@>
2020-02-11 22:14:46 +0100

    who knows

---
+++ src/components/content.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="pa2">
+  <div class="pa2 mw7 bg-white shadow tl">
+    <h2>About</h2>
+    <div>
+      <p>
+        I don't know how to write this without sounding to privileged. But every holiday that includes gift giving, family asks me and my girlfriend: "What do you want for *insert holiday here*", and we answer in unison "I don't know 😅". But in fact, we do know, but we don't remember it.
+      </p>
+      <p>
+        So I created this app. I also wanted to play around with SwiftUI, so that came as a bonus. But, back to what I was talking about. Wishywashy is a app for your wishlist, and it provides you with a way to share said list. That is! Nothing fancy, but it solves our problem 😁 Now my family knows what I want/need for birthdays and/or holidays
+      </p>
+      <p>
+        If you have any questions, please direct them to:
+        <a href="mailto:developer.wishywashy@gmail.com">developer.wishywashy@gmail.com</a>
+      </p>
+      <h2>Documents</h2>
+        <div>
+          <a href="/privacy_policy.html">Privacy policy</a>
+        </div>
+        <div>
+          <a href="/terms_and_conditions.html">Terms and Conditions</a>
+        </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ww-content"
+}
+</script>
+
+<style>
+  .shadow {
+    box-shadow:
+      3px 3px 0 #000,
+      -1px -1px 0 #000,  
+      1px -1px 0 #000,
+      -1px 1px 0 #000,
+      1px 1px 0 #000;
+  }
+</style>
