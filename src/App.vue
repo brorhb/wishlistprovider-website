cf26b3c25f28009ed3c51612716b473e3a5f3fcd
bror <@@>
2020-02-11 22:05:47 +0100

    pretty vue site

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="bg-wwpink flex justify-center">
+    <div class="flex flex-column">
+      <WwHeader></WwHeader>
+      <div class="pa2"></div>
+      <WwContent></WwContent>
+    </div>
+  </div>
+</template>
+
+<script>
+import WwHeader from "./components/header"
+import WwContent from "./components/content"
+export default {
+  name: 'App',
+  components: {
+    WwHeader,
+    WwContent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="pa2 flex flex-column white tl">
+    <div class="fw2 f4 f5-m subtitle">Remeber and share your wishes with</div>
+    <div class="fw6 f1 f2-m title">Wishywashy</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ww-header"
+}
+</script>
+
+<style>
+.title {
+  text-shadow:
+    3px 3px 0 #000,
+    -1px -1px 0 #000,  
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;
+}
+.subtitle {
+   text-shadow:
+      -1px -1px 0 #000,  
+      1px -1px 0 #000,
+      -1px 1px 0 #000,
+      1px 1px 0 #000;
+}
+</style>
